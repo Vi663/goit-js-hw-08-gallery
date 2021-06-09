4ae53ea4536001d1d3db66125cf22e93c7d1be9f
Вікторія Юрченко <viktoriau26@gmail.com>
2021-06-09 20:45:03 +0300

    +

--- src/gallery-items.js
+++ src/gallery-items.js
@@ -63,3 +63,75 @@ export default [
     description: 'Lighthouse Coast Sea',
   },
 ];
+import myExport from "./gallery-items.js";
+
+const galleryList = document.querySelector('.js-gallery');
+const modalWindow = document.querySelector('.js-lightbox');
+const modalWindowCloseBtnEl = document.querySelector('.lightbox__button');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+const bigImage = document.querySelector('.lightbox__image');
+
+function createList() {
+  myExport.map((image) => {
+    const listItemEl = document.createElement('li');
+    const linkEl = document.createElement('a');
+    const imgEl = document.createElement('img');
+    listItemEl.classList.add('gallery__item');
+    linkEl.classList.add('gallery__link');
+    linkEl.href = image.original;
+    imgEl.classList.add('gallery__image');
+    imgEl.src = image.preview;
+    imgEl.alt = image.description;
+  
+    linkEl.appendChild(imgEl);
+    listItemEl.appendChild(linkEl);
+    galleryList.appendChild(listItemEl);
+  });
+};
+createList()
+console.log(galleryList);
+
+function stopDefAction(event) {
+  event.preventDefault();
+  // console.log(linkElInList);
+}
+// const linkElInList = document.querySelectorAll('.gallery__link');
+  galleryList.addEventListener(
+    'click', stopDefAction
+);
+
+function onImageClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  modalWindow.classList.add('is-open');
+  bigImage.src = event.target.closest('.gallery__link').href;
+  bigImage.alt = event.target.alt;
+  console.log(modalWindow);
+  console.log(event.target);
+  console.log(bigImage);
+};
+galleryList.addEventListener("click", onImageClick);
+
+function closeTheOverlay(event) {
+  event.target.closest('.js-lightbox').classList.remove('is-open');
+  bigImage.src = '';
+  console.log(bigImage);
+};
+lightboxOverlay.addEventListener("click", closeTheOverlay);
+modalWindowCloseBtnEl.addEventListener("click", closeTheOverlay);
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && modalWindow.classList.contains('is-open')) {
+    modalWindow.classList.remove('is-open');
+    bigImage.src = '';
+  }
+  // console.log(e.key);
+});
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'ArrowLeft' && modalWindow.classList.contains('is-open')) {
+    bigImage.src = e.currentTarget.previousElementSibling.currentSrc;
+    console.log(bigImage.previousSibling);
+  } else if (e.key == 'ArrowRight' && modalWindow.classList.contains('is-open')) {
+      bigImage.src = bigImage.nextElementSibling.currentSrc;
+    }
+});
